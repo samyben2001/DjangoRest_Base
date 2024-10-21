200aeeb96af1f3f26ef531d9f14e40b18a049266
Samy ben beya <samyben2001@hotmail.com>
2024-10-21 20:02:57 +0200

    add requirements.txt

--- videoAPI/videoAPI/settings.py
+++ videoAPI/videoAPI/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import datetime
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,10 +89,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'DB_videoAPI',
-        'USER': 'postgres',
-        'PASSWORD': '.AZERTY67',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
